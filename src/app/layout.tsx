3a850b4535e006276565445fd2cca8ddd2d4b0a9
Jarrod Watts <jarrodwattsyt@gmail.com>
2024-09-24 12:16:13 +1000

    Initialize template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,22 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const avenueMono = localFont({
+  src: "./fonts/Avenue Mono.ttf",
+  variable: "--font-avenue-mono",
+  weight: "100, 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const roobert = localFont({
+  src: [
+    { path: "./fonts/Roobert-Light.ttf", weight: "300", style: "normal" },
+    { path: "./fonts/Roobert-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/Roobert-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/Roobert-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/Roobert-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/Roobert-Heavy.ttf", weight: "800", style: "normal" },
+  ],
+  variable: "--font-roobert",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${avenueMono.variable} ${roobert.variable} antialiased`}
       >
         {children}
       </body>
