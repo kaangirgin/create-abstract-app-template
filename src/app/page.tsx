f74b747aa11b59b5ebdcad103dcff2c2d2ff461d
Jarrod Watts <jarrodwattsyt@gmail.com>
2024-10-02 08:47:22 +1000

    Fix button hover styles on light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ export default function Home() {
               </div>
             ) : (
               <button
-                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-[family-name:var(--font-roobert)]"
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] hover:text-white dark:hover:bg-[#e0e0e0] dark:hover:text-black text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 font-[family-name:var(--font-roobert)]"
                 onClick={login}
               >
                 <Image
